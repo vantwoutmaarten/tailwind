2460f8f8c5eab557a777fa56a2a492c27b98ca06
vantwoutmaarten <maarten@vantwout.net>
2024-03-14 17:49:43 +0100

    fix: add examples of the most important tailwind flex props.

--- tailwind-practice/app/page.tsx
+++ tailwind-practice/app/page.tsx
@@ -46,6 +46,58 @@ export default function Home() {
       <div className="min-w-max h-16 bg-purple-500">box</div>
       <div className="min-w-max h-16 bg-purple-500">box</div>
       <div className="min-h-screen h-16 bg-yellow-500">box</div>
+      {/* flex */}
+      <div className="Parent">
+        <div className="bg-red-200">Header</div>
+        <div className="flex flex-row-reverse">
+          <div className="bg-red-200">Side bar</div>
+          <div className="bg-green-200 flex-grow">Main content</div>
+        </div>
+        <div className="flex flex-col bg-green-200">
+          <div className="">Side bar</div>
+          <div className="">Main content</div>
+        </div>
+        <div className="flex ">
+          <div className="basis-2/12">Side bar</div>
+          <div className="basis-8/12">Main content</div>
+          <div className="basis-2/12">Side bar</div>
+        </div>
+        <div className="flex flex-col md:flex-row">
+          <div className="w-32 bg-yellow-300">sidebar</div>
+          <div className="w-full bg-red-500">Main content</div>
+        </div>
+        <div className="flex justify-center">
+          <div className="">sidebar</div>
+          <div className="">Main content</div>
+        </div>
+        <div className="flex justify-between">
+          <div className="">sidebar</div>
+          <div className="">Main content</div>
+          <div className="">sidebar</div>
+        </div>
+        <div className="flex justify-evenly items-center">
+          <div className="bg-red-500">sidebar</div>
+          <div className="bg-green-500">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+            quaerat velit, possimus modi voluptatibus quas expedita repellendus
+            sunt minus quis minima fugiat, sed adipisci soluta cumque temporibus
+            beatae alias ipsa?
+          </div>
+          <div className="bg-red-500">sidebar</div>
+        </div>
+        <div className="flex justify-evenly items-baseline">
+          <div className="pt-2 bg-red-500">sidebar</div>
+          <div className="pt-4 bg-green-500">Lorem ipsum dolor sit amet</div>
+          <div className="pt-8 bg-red-500">sidebar</div>
+        </div>
+      </div>
+      {/* grid */}
+      <div className="grid grid-cols-4">
+        <div className="bg-sky-500 rounded-lg p-6">First Columns</div>
+        <div className="bg-sky-500 rounded-lg p-6">Second Columns</div>
+        <div className="bg-sky-500 rounded-lg p-6">Third Columns</div>
+        <div className="bg-sky-500 rounded-lg p-6">Fourth Columns</div>
+      </div>
     </>
   );
 }
